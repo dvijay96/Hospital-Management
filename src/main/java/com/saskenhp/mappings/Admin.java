8a8a4147178257bc2d09eafe1175b141050df896
Digvijay Manoj Parit <EE211385@sasken.com>
2020-03-31 18:13:17 +0530

    remote commit

--- src/main/java/com/saskenhp/mappings/Admin.java
+++ src/main/java/com/saskenhp/mappings/Admin.java
@@ -4,21 +4,20 @@ public interface Admin {
 
 	String reqURL = "/admin/secured/rest";
 
-	String addDoc = "/hp/addDoc";
+	String addEmp="/hp/addEmp";
+	
 	String editDoc = "/hp/editDoc";
-	String deleteDoc = "/hp/deleteDoc";
+	String deleteDoc = "/hp/deleteDoc/{id}";
 	String docByName = "hp/doc/{name}";
 	String listDoc = "/hp/allDocs";
 
-	String addRecp = "/hp/recp/add";
 	String editRecp = "hp/recp/edit";
-	String deleteRecp = "hp/recp/delete";
+	String deleteRecp = "hp/recp/delete/{id}";
 	String recpByName = "hp/recp/{name}";
 	String listRecp = "hp/resp/all";
 
-	String addPatient = "/hp/patient/add";
 	String editPatient = "hp/patient/edit";
-	String deletePatient = "hp/patient/delete";
+	String deletePatient = "hp/patient/delete/{id}";
 	String patientByName = "hp/patient/{name}";
 	String listPatient = "hp/patient/all";
 
--- src/main/java/com/saskenhp/repositories/EmployeeRepo.java
+++ src/main/java/com/saskenhp/repositories/EmployeeRepo.java
@@ -1,11 +1,20 @@
 package com.saskenhp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.saskenhp.entity.Employee;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+	@Query("FROM Employee WHERE firstName=?1")
+	public Employee findByName(String firstName);
+
+	@Query("FROM Employee e inner join e.role r where r.role=:role")
+	public List<Employee> findAllDoctors(String role);
 
 }
